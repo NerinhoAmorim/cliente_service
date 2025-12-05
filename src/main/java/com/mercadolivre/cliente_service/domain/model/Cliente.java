c896be494199b1d1d8af022085d3c29ff3281fda
Nerinho Amorim Nascimento <156155459+NerinhoAmorim@users.noreply.github.com>
2025-12-04 23:57:25 -0300

    primeiro commit

---
+++ src/main/java/com/mercadolivre/cliente_service/domain/model/Cliente.java
@@ -0,0 +1,44 @@
+package com.mercadolivre.cliente_service.domain.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cliente {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false, length = 150)
+    private String email;
+
+    private LocalDate dataNascimento;
+
+    @Column(length = 20)
+    private String telefone;
+	}
+
